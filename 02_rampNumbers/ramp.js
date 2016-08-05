dbb1c08fe69bb563d4aec4a3222c6a456921707e
bfield5 <Admin@brians-MacBook-Pro.local>
2016-08-04 18:43:01 -0600

    ramp numbers

---
+++ 02_rampNumbers/ramp.js
@@ -0,0 +1,16 @@
+var stringCount = "1234";
+
+}
+function findRamp (stringToBeCounted) {
+  var i;
+  var count = 0;
+  for (i = 0; i < stringToBeCounted.length; i += 1) {
+
+    if (stringToBeCounted[i] <  ) {
+      Count += stringToBeCounted[i];
+    }
+    return count;
+  }
+}
+var count = findRamp (stringCount);
+console.log(count);
